8bc0caa5932c797db241a8fafd80f89ed5ebddda
Venkata Ramanan <61214578+vramananx@users.noreply.github.com>
2025-02-04 09:15:17 -0800

    add refresh

---
+++ Software/RF2DFieldSolver/CustomWidgets/pcbview.cpp
@@ -0,0 +1,439 @@
+#include "pcbview.h"
+
+#include <QPainter>
+#include <QMouseEvent>
+#include <QContextMenuEvent>
+#include <QMenu>
+#include <QAction>
+
+#include "ui_vertexEditDialog.h"
+#include "util.h"
+
+#include "polygon.h"
+
+const QColor PCBView::backgroundColor = Qt::lightGray;
+const QColor PCBView::GNDColor = Qt::black;
+const QColor PCBView::tracePosColor = Qt::red;
+const QColor PCBView::traceNegColor = Qt::blue;
+const QColor PCBView::dielectricColor = Qt::darkGreen;
+const QColor PCBView::gridColor = Qt::gray;
+
+PCBView::PCBView(QWidget *parent)
+    : QWidget{parent}
+{
+    list = nullptr;
+    laplace = nullptr;
+    topLeft = QPointF(-1, 1);
+    topLeft = QPointF(1, -1);
+    appendElement = nullptr;
+    dragVertex.e = nullptr;
+    dragVertex.index = 0;
+    pressCoordsValid = false;
+    grid = 1e-4;
+    showGrid = false;
+    snapToGrid = false;
+    showPotential = false;
+    keepAspectRatio = true;
+}
+
+void PCBView::setCorners(QPointF topLeft, QPointF bottomRight)
+{
+    this->topLeft = topLeft;
+    this->bottomRight = bottomRight;
+}
+
+void PCBView::setElementList(ElementList *list)
+{
+    this->list = list;
+}
+
+void PCBView::setLaplace(Laplace *laplace)
+{
+    this->laplace = laplace;
+}
+
+void PCBView::startAppending(Element *e)
+{
+    appendElement = e;
+    setMouseTracking(true);
+}
+
+void PCBView::setGrid(double grid)
+{
+    this->grid = grid;
+    update();
+}
+
+void PCBView::setShowGrid(bool show)
+{
+    showGrid = show;
+    update();
+}
+
+void PCBView::setSnapToGrid(bool snap)
+{
+    snapToGrid = snap;
+}
+
+void PCBView::setShowPotential(bool show)
+{
+    showPotential = show;
+    update();
+}
+
+void PCBView::setKeepAspectRatio(bool keep)
+{
+    keepAspectRatio = keep;
+    update();
+}
+
+void PCBView::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event)
+
+    QPainter p(this);
+
+    p.setViewport(0, 0, width(), height());
+    // p.setWindow is limited to integers, this implementation works with floating point
+    transform = QTransform();
+    if(keepAspectRatio) {
+        // figure out which ratio is the limiting factor
+        auto x_ratio = p.viewport().width() / (bottomRight.x() - topLeft.x());
+        auto y_ratio = p.viewport().height() / (bottomRight.y() - topLeft.y());
+        auto ratio = std::min(abs(x_ratio), abs(y_ratio));
+        transform.scale(copysign(ratio, x_ratio), copysign(ratio, y_ratio));
+
+        auto above = (topLeft.y() - bottomRight.y()) * (abs(y_ratio) / ratio - 1) / 2;
+        auto left = (topLeft.x() - bottomRight.x()) * (abs(x_ratio) / ratio - 1) / 2;
+
+        transform.translate(-topLeft.x() - left, -topLeft.y() - above);
+    } else {
+        transform.scale(p.viewport().width() / (bottomRight.x() - topLeft.x()),
+                        p.viewport().height() / (bottomRight.y() - topLeft.y()));
+        transform.translate(-topLeft.x(), -topLeft.y());
+    }
+
+    p.fillRect(QRectF(transform.map(topLeft), transform.map(bottomRight)), Qt::lightGray);
+    p.setBackground(QBrush(Qt::black));
+
+    // show potential field
+    // TODO make this optional
+    if(showPotential && laplace && laplace->isResultReady()) {
+        for(int i=0;i<width();i++) {
+            for(int j=0;j<height();j++) {
+                auto coord = transform.inverted().map(QPointF(i, j));
+                auto v = laplace->getPotential(coord);
+                p.setPen(Util::getIntensityGradeColor(v));
+                p.setOpacity(sqrt(abs(v)));
+                p.drawPoint(i, j);
+            }
+        }
+    }
+    p.setOpacity(1.0);
+
+    // draw grid
+    if(showGrid) {
+        // x axis
+        p.setPen(gridColor);
+        for(double x = snapToGridPoint(topLeft).x(); x < bottomRight.x(); x += grid) {
+            auto top = transform.map(QPointF(x, topLeft.y()));
+            auto bottom = transform.map(QPointF(x, bottomRight.y()));
+            p.drawLine(top, bottom);
+        }
+        // y axis
+        for(double y = snapToGridPoint(bottomRight).y(); y < topLeft.y(); y += grid) {
+            auto left = transform.map(QPointF(topLeft.x(), y));
+            auto right = transform.map(QPointF(bottomRight.x(), y));
+            p.drawLine(left, right);
+        }
+    }
+
+    // Show elements
+    if(list) {
+        for(auto e : list->getElements()) {
+            QColor elementColor;
+            switch(e->getType()) {
+            case Element::Type::Dielectric: elementColor = dielectricColor; break;
+            case Element::Type::TracePos: elementColor = tracePosColor; break;
+            case Element::Type::TraceNeg: elementColor = traceNegColor; break;
+            case Element::Type::GND: elementColor = GNDColor; break;
+            default: elementColor = Qt::gray; break;
+            }
+            p.setBrush(elementColor);
+            p.setPen(elementColor);
+
+            auto vertices = e->getVertices();
+            // paint vertices in viewport to get constant vertex size
+            for(auto v : vertices) {
+                auto devicePoint = transform.map(v);
+                p.drawEllipse(devicePoint, vertexSize/2, vertexSize/2);
+            }
+            // draw connections between vertices
+            if(vertices.size() > 1) {
+                for(unsigned int i=0;i<vertices.size();i++) {
+                    int prev = i-1;
+                    if(prev < 0) {
+                        if(e == appendElement) {
+                            // we are appending to this element, do not draw last line in polygon
+                            continue;
+                        }
+                        prev = vertices.size() -  1;
+                    }
+                    QPointF start = transform.map(vertices[i]);
+                    QPointF stop = transform.map(vertices[prev]);
+                    p.drawLine(start, stop);
+                }
+            }
+            if(vertices.size() > 0 && e == appendElement){
+                        // draw line from last vertex to pointer
+                        QPointF start = transform.map(vertices[vertices.size()-1]);
+                        QPointF stop = lastMouseCoords;
+                        if(snapToGrid) {
+                            stop = transform.map(snapToGridPoint(transform.inverted().map(stop)));
+                        }
+                        p.drawLine(start, stop);
+            }
+        }
+    }
+}
+
+void PCBView::mousePressEvent(QMouseEvent *event)
+{
+    if (appendElement) {
+        // check if we clicked on the first vertex
+        auto vertices = appendElement->getVertices();
+        if(vertices.size() > 0 && getPixelDistanceToVertex(event->pos(), vertices[0]) < vertexCatchRadius) {
+            // clicked on the first element again, abort append mode
+            appendElement = nullptr;
+            setMouseTracking(false);
+            update();
+        } else {
+            // record coordinates to place vertex when the mouse is released
+            pressCoords = event->pos();
+            lastMouseCoords = pressCoords;
+            pressCoordsValid = true;
+        }
+    } else {
+        // not appending, may have been a click on a vertex
+        dragVertex = catchVertex(event->pos());
+    }
+}
+
+void PCBView::mouseReleaseEvent(QMouseEvent *event)
+{
+    Q_UNUSED(event);
+    if (appendElement && pressCoordsValid) {
+        // add vertex at indicated coordinates
+        auto vertexPoint = transform.inverted().map(QPointF(pressCoords));
+        if(snapToGrid) {
+            vertexPoint = snapToGridPoint(vertexPoint);
+        }
+        appendElement->appendVertex(vertexPoint);
+        someElementChanged();
+        pressCoordsValid = false;
+        update();
+    } else if (dragVertex.e) {
+        dragVertex.e = nullptr;
+    }
+}
+
+void PCBView::mouseMoveEvent(QMouseEvent *event)
+{
+    if (appendElement) {
+        // ignore, just record mouse position
+        lastMouseCoords = event->pos();
+        update();
+    } else if(dragVertex.e) {
+        // dragging a vertex
+        auto vertexPoint = transform.inverted().map(QPointF(event->pos()));
+        if(snapToGrid) {
+            vertexPoint = snapToGridPoint(vertexPoint);
+        }
+        dragVertex.e->changeVertex(dragVertex.index, vertexPoint);
+        someElementChanged();
+        update();
+    }
+}
+
+void PCBView::mouseDoubleClickEvent(QMouseEvent *event)
+{
+    if (appendElement) {
+        // double-click aborts appending
+        appendElement = nullptr;
+        setMouseTracking(false);
+        update();
+    } else {
+        auto info = catchVertex(event->pos());
+        if(info.e) {
+            // edit vertex coordinates
+            auto d = new QDialog(this);
+            d->setAttribute(Qt::WA_DeleteOnClose);
+            auto ui = new Ui::VertexEditDialog;
+            ui->setupUi(d);
+
+            // save previous coordinates
+            auto oldCoords = info.e->getVertices()[info.index];
+
+            auto updateVertex = [=](const QPointF &p){
+                info.e->changeVertex(info.index, p);
+                update();
+            };
+
+            ui->xpos->setUnit("m");
+            ui->xpos->setPrefixes("um ");
+            ui->xpos->setPrecision(4);
+            ui->xpos->setValue(oldCoords.x());
+            connect(ui->xpos, &SIUnitEdit::valueChanged, this, [=](){
+                updateVertex(QPointF(ui->xpos->value(), ui->ypos->value()));
+            });
+
+            ui->ypos->setUnit("m");
+            ui->ypos->setPrefixes("um ");
+            ui->ypos->setPrecision(4);
+            ui->ypos->setValue(oldCoords.y());
+            connect(ui->ypos, &SIUnitEdit::valueChanged, this, [=](){
+                updateVertex(QPointF(ui->xpos->value(), ui->ypos->value()));
+            });
+
+            connect(ui->buttonBox, &QDialogButtonBox::accepted, d, &QDialog::accept);
+            connect(ui->buttonBox, &QDialogButtonBox::rejected, this, [=](){
+                // restore old coordinates
+                info.e->changeVertex(info.index, oldCoords);
+                update();
+                d->reject();
+            });
+
+            d->show();
+        }
+    }
+}
+
+void PCBView::contextMenuEvent(QContextMenuEvent *event)
+{
+    if (appendElement) {
+        // ignore
+        return;
+    }
+    auto menu = new QMenu();
+    auto infoVertex = catchVertex(event->pos());
+    auto infoLine = catchLine(event->pos());
+    Element *e = nullptr;
+    if(infoVertex.e) {
+        e = infoVertex.e;
+        // clicked on a vertex
+        auto actionDeleteVertex = new QAction("Delete Vertex");
+        menu->addAction(actionDeleteVertex);
+        connect(actionDeleteVertex, &QAction::triggered, [=](){
+            infoVertex.e->removeVertex(infoVertex.index);
+            someElementChanged();
+        });
+    } else if(infoLine.e) {
+        e = infoLine.e;
+        // clicked on a line
+        auto actionInsertVertex = new QAction("Insert Vertex here");
+        menu->addAction(actionInsertVertex);
+        connect(actionInsertVertex, &QAction::triggered, [=](){
+            // figure out at which index we have to insert the new vertex.
+            // Usually this is the higher of the two but there is a special case if one index is the first vertex and the other the last
+            int insertIndex = std::max(infoLine.index1, infoLine.index2);
+            if((infoLine.index1 == 0 && infoLine.index2 == infoLine.e->getVertices().size() - 1)
+                    || (infoLine.index1 == 0 && infoLine.index2 == infoLine.e->getVertices().size() - 1)) {
+                // special case
+                insertIndex++;
+            }
+            auto vertexPoint = transform.inverted().map(QPointF(event->pos()));
+            infoLine.e->addVertex(insertIndex, vertexPoint);
+            someElementChanged();
+        });
+    }
+    // TODO check if connection between vertices was clicked
+    if(e) {
+        // clicked on something connected to an element
+        auto actionDeleteElement = new QAction("Delete Element");
+        menu->addAction(actionDeleteElement);
+        connect(actionDeleteElement, &QAction::triggered, [=](){
+            list->removeElement(e);
+            someElementChanged();
+        });
+    }
+    menu->exec(event->globalPos());
+    update();
+}
+
+double PCBView::getPixelDistanceToVertex(QPoint cursor, QPointF vertex)
+{
+    // convert vertex into pixel coordinates
+    QPoint vertexPixel = transform.map(vertex).toPoint();
+    auto diff = vertexPixel - cursor;
+    return std::sqrt(diff.x()*diff.x()+diff.y()*diff.y());
+}
+
+void PCBView::someElementChanged()
+{
+    if(laplace && laplace->isResultReady()) {
+        laplace->invalidateResult();
+    }
+}
+
+PCBView::VertexInfo PCBView::catchVertex(QPoint cursor)
+{
+    VertexInfo info;
+    info.e = nullptr;
+    double closestDistance = vertexCatchRadius;
+    for(auto e : list->getElements()) {
+        for(unsigned int i=0;i<e->getVertices().size();i++) {
+            auto distance = getPixelDistanceToVertex(cursor, e->getVertices()[i]);
+            if(distance < closestDistance) {
+                closestDistance = distance;
+                info.e = e;
+                info.index = i;
+            }
+        }
+    }
+    return info;
+}
+
+PCBView::LineInfo PCBView::catchLine(QPoint cursor)
+{
+    LineInfo info;
+    info.e = nullptr;
+    double closestDistance = vertexCatchRadius;
+    for(auto e : list->getElements()) {
+        if(e->getVertices().size() < 2) {
+            continue;
+        }
+        for(unsigned int i=0;i<e->getVertices().size();i++) {
+            int prev = (int) i - 1;
+            if(prev < 0) {
+                prev = e->getVertices().size() - 1;
+            }
+            QPointF vertexPixel1 = transform.map(e->getVertices()[i]).toPoint();
+            QPointF vertexPixel2 = transform.map(e->getVertices()[prev]).toPoint();
+            auto distance = Util::distanceToLine(QPointF(cursor), vertexPixel1, vertexPixel2);
+            if(distance < closestDistance) {
+                closestDistance = distance;
+                info.e = e;
+                info.index1 = i;
+                info.index2 = prev;
+            }
+        }
+    }
+    return info;
+}
+
+QPointF PCBView::getBottomRight() const
+{
+    return bottomRight;
+}
+
+QPointF PCBView::getTopLeft() const
+{
+    return topLeft;
+}
+
+QPointF PCBView::snapToGridPoint(const QPointF &pos)
+{
+    double snap_x = std::round(pos.x() / grid) * grid;
+    double snap_y = std::round(pos.y() / grid) * grid;
+    return QPointF(snap_x, snap_y);
+}
---
+++ Software/RF2DFieldSolver/laplace/laplace.cpp
@@ -0,0 +1,273 @@
+#include "laplace.h"
+
+#include <QPolygonF>
+
+Laplace::Laplace(QObject *parent)
+    : QObject{parent}
+{
+    calculationRunning = false;
+    resultReady = false;
+    list = nullptr;
+    grid = 1e-5;
+    threads = 1;
+    threshold = 1e-6;
+    lattice = nullptr;
+    groundedBorders = true;
+    ignoreDielectric = false;
+}
+
+void Laplace::setArea(const QPointF &topLeft, const QPointF &bottomRight)
+{
+    if(calculationRunning) {
+        return;
+    }
+    this->topLeft = topLeft;
+    this->bottomRight = bottomRight;
+}
+
+void Laplace::setGrid(double grid)
+{
+    if(calculationRunning) {
+        return;
+    }
+    if(grid > 0) {
+        this->grid = grid;
+    }
+}
+
+void Laplace::setThreads(int threads)
+{
+    if(calculationRunning) {
+        return;
+    }
+    if(threads > 0) {
+        this->threads = threads;
+    }
+}
+
+void Laplace::setThreshold(double threshold)
+{
+    if(calculationRunning) {
+        return;
+    }
+    if (threshold > 0) {
+        this->threshold = threshold;
+    }
+}
+
+void Laplace::setGroundedBorders(bool gnd)
+{
+    if(calculationRunning) {
+        return;
+    }
+    groundedBorders = gnd;
+}
+
+void Laplace::setIgnoreDielectric(bool ignore)
+{
+    if(calculationRunning) {
+        return;
+    }
+    ignoreDielectric = ignore;
+}
+
+bool Laplace::startCalculation(ElementList *list)
+{
+    if(calculationRunning) {
+        return false;
+    }
+    calculationRunning = true;
+    resultReady = false;
+    lastPercent = 0;
+    emit info("Laplace calculation starting");
+    if(lattice) {
+        delete lattice;
+        lattice = nullptr;
+    }
+    this->list = list;
+
+    // start the calculation thread
+    auto err = pthread_create(&thread, nullptr, calcThreadTrampoline, this);
+    if(err) {
+        emit error("Failed to start laplace thread");
+        return false;
+    }
+    emit info("Laplace thread started");
+    return true;
+}
+
+void Laplace::abortCalculation()
+{
+    if(!calculationRunning) {
+        return;
+    }
+    // request abort of calculation
+    lattice->abort = true;
+}
+
+double Laplace::getPotential(const QPointF &p)
+{
+    if(!resultReady) {
+        return std::numeric_limits<double>::quiet_NaN();
+    }
+    auto pos = coordToRect(p);
+    // convert to integers and shift by the added outside boundary of NaNs
+    int index_x = round(pos.x) + 1;
+    int index_y = round(pos.y) + 1;
+    if(index_x < 0 || index_x >= (int) lattice->dim.x || index_y < 0 || index_y >= (int) lattice->dim.y) {
+        return std::numeric_limits<double>::quiet_NaN();
+    }
+    auto c = &lattice->cells[index_x+index_y*lattice->dim.x];
+    return c->value;
+}
+
+QLineF Laplace::getGradient(const QPointF &p)
+{
+    QLineF ret = QLineF(p, p);
+    if(!resultReady) {
+        return ret;
+    }
+    auto pos = coordToRect(p);
+    // convert to integers and shift by the added outside boundary of NaNs
+    int index_x = floor(pos.x) + 1;
+    int index_y = floor(pos.y) + 1;
+
+    if(index_x < 0 || index_x + 1 >= (int) lattice->dim.x || index_y < 0 || index_y + 1>= (int) lattice->dim.y) {
+        return ret;
+    }
+    // calculate gradient
+    auto c_floor = &lattice->cells[index_x+index_y*lattice->dim.x];
+    auto c_x = &lattice->cells[index_x+1+index_y*lattice->dim.x];
+    auto c_y = &lattice->cells[index_x+(index_y+1)*lattice->dim.x];
+    auto grad_x = c_x->value - c_floor->value;
+    auto grad_y = c_y->value - c_floor->value;
+    ret.setP2(p + QPointF(grad_x, grad_y));
+    return ret;
+}
+
+void Laplace::invalidateResult()
+{
+    resultReady = false;
+}
+
+QPointF Laplace::coordFromRect(rect *pos)
+{
+    QPointF ret;
+    ret.rx() = pos->x * grid + topLeft.x();
+    ret.ry() = pos->y * grid + bottomRight.y();
+    return ret;
+}
+
+struct rect Laplace::coordToRect(const QPointF &pos)
+{
+    struct rect ret;
+    ret.x = (pos.x() - topLeft.x()) / grid;
+    ret.y = (pos.y() - bottomRight.y()) / grid;
+    return ret;
+}
+
+bound *Laplace::boundary(bound *bound, rect *pos)
+{
+    auto coord = coordFromRect(pos);
+    bound->value = 0;
+    bound->cond = NONE;
+
+    bool isBorder = qFuzzyCompare(coord.x(), topLeft.x()) || qFuzzyCompare(coord.x(), bottomRight.x()) || qFuzzyCompare(coord.y(), topLeft.y()) || qFuzzyCompare(coord.y(), bottomRight.y());
+
+    // handle borders
+    if(groundedBorders && isBorder) {
+        bound->value = 0;
+        bound->cond = DIRICHLET;
+        return bound;
+    } else {
+        // find the matching polygon
+        for(auto e : list->getElements()) {
+            if(e->getType() == Element::Type::Dielectric) {
+                // skip, dielectric has no influence on boundary and trace/GND should always take priority
+                continue;
+            }
+            QPolygonF poly = e->toPolygon();
+            if(poly.containsPoint(coord, Qt::OddEvenFill)) {
+                // this polygon defines the boundary at these coordinates
+                switch(e->getType()) {
+                case Element::Type::GND:
+                    bound->value = 0;
+                    bound->cond = DIRICHLET;
+                    return bound;
+                case Element::Type::TracePos:
+                    bound->value = 1.0;
+                    bound->cond = DIRICHLET;
+                    return bound;
+                case Element::Type::TraceNeg:
+                    bound->value = -1.0;
+                    bound->cond = DIRICHLET;
+                    return bound;
+                case Element::Type::Dielectric:
+                case Element::Type::Last:
+                    return bound;
+                }
+            }
+        }
+    }
+    return bound;
+}
+
+double Laplace::weight(rect *pos)
+{
+    if(ignoreDielectric) {
+        return 1.0;
+    }
+
+    auto coord = coordFromRect(pos);
+    return sqrt(list->getDielectricConstantAt(coord));
+}
+
+void* Laplace::calcThread()
+{
+    emit info("Creating lattice");
+    struct rect size = {(bottomRight.x() - topLeft.x()) / grid, (topLeft.y() - bottomRight.y()) / grid};
+    struct point dim = {(uint32_t) ((bottomRight.x() - topLeft.x()) / grid), (uint32_t) ((topLeft.y() - bottomRight.y()) / grid)};
+    lattice = lattice_new(&size, &dim, &boundaryTrampoline, &weightTrampoline, this);
+    if(lattice) {
+        emit info("Lattice creation complete");
+    } else {
+        emit error("Lattice creation failed");
+        return nullptr;
+    }
+
+    struct config conf = {(uint8_t) threads, 10, threshold};
+    if(conf.threads > lattice->dim.y / 5) {
+        conf.threads = lattice->dim.y / 5;
+    }
+    conf.distance = lattice->dim.y / threads;
+    emit info("Starting calculation threads");
+    auto it = lattice_compute_threaded(lattice, &conf, calcProgressFromDiffTrampoline, this);
+    calculationRunning = false;
+    if(lattice->abort) {
+        emit warning("Laplace calculation aborted");
+        resultReady = false;
+        emit percentage(0);
+        emit calculationAborted();
+    } else {
+        emit info("Laplace calculation complete, took "+QString::number(it)+" iterations");
+        resultReady = true;
+        emit percentage(100);
+        emit calculationDone();
+    }
+    return nullptr;
+}
+
+void Laplace::calcProgressFromDiff(double diff)
+{
+    // diff is expected to go down from 1.0 to the threshold with exponetial decay
+    double endTime = pow(-log(threshold), 6);
+    double currentTime = pow(-log(diff), 6);
+    double percent = currentTime * 100 / endTime;
+    if(percent > 100) {
+        percent = 100;
+    } else if(percent < lastPercent) {
+        percent = lastPercent;
+    }
+    lastPercent = percent;
+    emit percentage(percent);
+}
